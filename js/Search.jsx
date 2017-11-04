b604321135b213176570ae629e20b0ebef124533
Giulio Grillanda <ggrillanda@trulia.com>
2017-11-03 17:23:26 -0700

    end of day1

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import preload from '../data.json';
+import ShowCard from './ShowCard';
 
-const Search = () => <h1>hi lol this is Search</h1>;
+class Search extends Component {
+  state = {
+    searchTerm: ''
+  };
+
+  handleSearchTermChange = event => {
+    this.setState({
+      searchTerm: event.target.value
+    });
+  };
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+            name=""
+            id=""
+            onChange={this.handleSearchTermChange}
+          />
+        </header>
+        <div>
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imbdId} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
